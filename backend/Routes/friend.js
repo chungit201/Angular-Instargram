a17c6de01fc624ab87ab7113ec9a52253b59d272
Tran Quang <kysumuoiba@teneocto.io>
2021-10-14 15:08:38 +0700

    update follow

--- backend/Routes/friend.js
+++ backend/Routes/friend.js
@@ -1,23 +1,23 @@
-import express from 'express';
+import express from "express";
 import {
   addFriend,
   removeFriend,
   friendID,
   updateFriend,
   listFriend,
-  findFriendToUser
-} from '../Controllers/friendControllers.js';
-import {
-  authToken
-} from '../Middleware/authenToken'
+  findFriendToUser,
+  updateClearfriend,
+} from "../Controllers/friendControllers.js";
+import { authToken } from "../Middleware/authenToken";
 const router = express.Router();
 
-router.get('/friend/find-user', authToken, findFriendToUser)
-router.post('/add-friend', authToken, addFriend);
-router.put('/friend/update/:friendID', authToken, updateFriend);
-router.delete('/friend/remove/:friendID', authToken, removeFriend);
-router.get('/friend', listFriend);
+router.get("/friend/find-user", authToken, findFriendToUser);
+router.post("/add-friend", authToken, addFriend);
+router.put("/friend/update/:friendID", authToken, updateFriend);
+router.put("/friend/clear/:friendID", authToken, updateClearfriend);
+router.delete("/friend/remove/:friendID", authToken, removeFriend);
+router.get("/friend", listFriend);
 
-router.param('friendID', friendID);
+router.param("friendID", friendID);
 
 module.exports = router;
