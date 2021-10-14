04236794d383aa86139af1c589689796fe62b933
chungit201 <63365979+chungit201@users.noreply.github.com>
2021-10-14 09:28:13 +0700

    updatesLikeUser

--- src/app/screen/main/posts/like-by-user/like-by-user.component.ts
+++ src/app/screen/main/posts/like-by-user/like-by-user.component.ts
@@ -20,11 +20,41 @@ export class LikeByUserComponent implements OnChanges, OnInit {
   ngOnInit(): void {
     this.getUserLike();
   }
+  listLike(e:any){
+    console.log("okoko")
+    e.preventDefault()
+     
+     const likesUser = document.querySelector('.like_user') as HTMLElement;
+     let btnUserLike = document.querySelectorAll('#listLike');    
+     const overBg = document.querySelector('#overBg') as HTMLElement
+     overBg.style.backgroundColor = 'black';
+       overBg.style.opacity = "0.5";
+       overBg.style.position = 'fixed'
+       likesUser.style.display = 'block';
+   }
+   outlike(){
+    const likesUser = document.querySelector('.like_user') as HTMLElement;
+    const overBg = document.querySelector('#overBg') as HTMLElement
+    likesUser.style.display = 'none';
+    overBg.style.opacity = "0";
+    overBg.style.position = ''
+   }
+   clearBox(e:any){
+     e.preventDefault()
+     const likesUser = document.querySelector('.like_user') as HTMLElement;
+     let btnUserLike = document.querySelectorAll('#listLike');   
+     const overBg = document.querySelector('#overBg') as HTMLElement
+     likesUser.style.display = 'none';
+     overBg.style.backgroundColor ='';
+     overBg.style.position= ''
+   }
 
   private getUserLike(): void {
     const { like } = this.itemPost;
     this.userService.profile(like.user[0]).subscribe((data) => {
       const { name }: any = data;
+      console.log(data);
+      
       if (like.amount == 1) {
         this.textLike = name;
         return;
